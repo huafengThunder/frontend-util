d8f96985efe735f65465876171e50ebcc18f771c
lhf <huafenglei@risinghf.com>
2022-11-29 00:05:41 +0800

    content

--- src/platform/pc/layout/frontend-nav/header/index.jsx
+++ src/platform/pc/layout/frontend-nav/header/index.jsx
@@ -5,8 +5,8 @@ function Header() {
   return (
     <div className={styles.header}>
       <section className={styles.left}>
-        <h1>前端导航</h1>
-        <h1 style={{color:'silver'}}>博客</h1>
+        <h1 className={styles.active}>前端导航</h1>
+        <h1>博客</h1>
       </section>
       <section className={styles.center}></section>
       <section className={styles.right}></section>
