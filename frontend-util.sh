479586d22e8f0ed587d5d996fb3407a20b8685d5
lhf <huafenglei@risinghf.com>
2023-02-15 23:11:41 +0800

    mistask

--- frontend-util.sh
+++ frontend-util.sh
@@ -7,12 +7,12 @@ containerName=frontend-util-80
 ## 重新创建容器
 
 # 停止容器
-docker stop $containerName &&
+docker stop ${containerName} &&
 # 删除容器
-docker rm $containerName &&
+docker rm ${containerName} &&
 # 删除镜像
-docker rmi $imageName &&
+docker rmi ${imageName} &&
 # 重新构建镜像
-docker build -t $imageName . &&
+docker build -t ${imageName} . &&
 # 重新创建容器
-docker run -d -p $port:80 --name $containerName $imageName
+docker run -d -p ${port}:80 --name ${containerName} ${imageName}
