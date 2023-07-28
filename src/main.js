1ceb065624e65ec4354f2f8365594e60fcd11ead
unknown <437832424@qq.com>
2023-07-28 23:46:15 +0800

    注释vConsole

--- src/main.js
+++ src/main.js
@@ -5,11 +5,8 @@ const PcApp = lazy(() => import('./platform/pc/app.jsx'));
 const MobileApp = lazy(() => import('./platform/mobile/app.jsx'));
 import './style/reset.less'
 const root = ReactDOM.createRoot(document.getElementById('root'))
-import VConsole from 'vconsole';
-if (judgePlatform()) {
-    const vConsole = new VConsole();
-    // 接下来即可照常使用 `console` 等方法
-    console.log('Hello world');
-    // vConsole.destroy();
-}
+// import VConsole from 'vconsole';
+// const vConsole = new VConsole();
+// 关闭调试
+// vConsole.destroy();
 root.render(!judgePlatform() ? <PcApp /> : <MobileApp />)
