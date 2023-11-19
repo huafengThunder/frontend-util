ccd66a6534d637c217490cc1fa4c02ebacc06fb0
unknown <437832424@qq.com>
2023-11-19 20:04:47 +0800

    bug map obejct is null

--- src/platform/pc/views/memorandum/index.jsx
+++ src/platform/pc/views/memorandum/index.jsx
@@ -29,7 +29,7 @@ export default function Memorandum() {
     return (
         <div className={styles.memorandum}>
             <section className={styles.title}>
-                <FolderList list={list} setCurItem={setCurItem} setList={setList} />
+                <FolderList list={list || []} setCurItem={setCurItem} setList={setList} />
             </section>
             <section className={styles.content}>
                 {list.length > 0 && <Content item={curItem} noEdit={noEdit} setNoEdit={setNoEdit} setList={setList} />}
@@ -52,7 +52,7 @@ const FolderList = React.memo((props) => {
     return (
         <Box sx={{ width: '300px', bgcolor: 'background.paper' }}>
             <List sx={{ padding: 0 }}>
-                {list?.map(item => (
+                {list.map(item => (
                     <ListItem disablePadding
                         key={item.id}
                         onClick={() => handleClick(item)}
