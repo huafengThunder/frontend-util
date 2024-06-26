8d5067b677f859ed62c0f1f237b2b11cbd2de9e4
huafengThunder <437832424@qq.com>
2024-06-26 22:12:23 +0800

    对接管理员可新增菜单

--- src/api/menu.js
+++ src/api/menu.js
@@ -1,8 +1,18 @@
 import requests from '@/utils/request';
 export function getMenus(data) {
     return requests({
-        url: '/feutil/menu/',
+        url: '/feutil/menu',
         method: 'get',
         params: data
+    });
+}
+export function addMenuApi(data) {
+    return requests({
+        url: '/feutil/menu',
+        method: 'post',
+        data: data,
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
     });
 }
--- src/platform/pc/components/OperMenu/index.jsx
+++ src/platform/pc/components/OperMenu/index.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { Button, Dialog, DialogTitle, DialogContent, TextField, DialogActions } from '@mui/material';
+import { addMenuApi } from '@/api/menu.js';
+import { useNavigate } from 'react-router-dom'
 
 export const EditMenuDialog = ({ open, handleClose, menuData, setMenuData }) => {
     const [formData, setFormData] = useState(menuData || {
@@ -14,9 +16,13 @@ export const EditMenuDialog = ({ open, handleClose, menuData, setMenuData }) => 
         setFormData({ ...formData, [e.target.name]: e.target.value });
     };
 
-    const handleSave = () => {
-        setMenuData(formData);
-        handleClose();
+    const handleSave = async () => {
+        try {
+            await addMenuApi(formData)
+            handleClose();
+            // 体验不好，后面可以借助redux（eventBus容易产生魔法代码）
+            useNavigate()(0)
+        } catch (error) { }
     };
 
     return (
